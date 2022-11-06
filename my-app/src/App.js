ae7867c118a3efbe0083e8e34d3075d3411236df
Athif binu <binuathif@gmail.com>
2022-11-06 23:36:13 +0530

    to Start

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "./componets/NavBar/NavBar";
+import './App.css'
+
+
+function App() {
+  return (
+     <div className="App">
+        <NavBar/>
+     </div>
+  );
+}
+
+export default App;
